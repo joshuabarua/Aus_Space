0de1a93653361eed59d9a2b08cf2aaf46e64dee2
Unknown <joshuabarua9@hotmail.com>
2020-04-10 15:54:04 +0100

    Devise/Ruby Admin installed and functional

--- Gemfile
+++ Gemfile
@@ -67,3 +67,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'rails-controller-testing'
 
 gem 'devise'
+
+gem 'rails_admin'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users, skip: :registrations
   root 'static_pages#home'
   get  '/home',      to: 'static_pages#home'
   get  '/help',      to: 'static_pages#help'
   get  '/about',     to: 'static_pages#about'
   get  '/contact',   to: 'static_pages#contact'
+  get  '/admin',     to: 'devise#sessions/new'
 
 end
